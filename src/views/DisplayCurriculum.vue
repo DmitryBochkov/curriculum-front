4f9adb5e8d91259f125870b45c269306b946a9c9
DmitryBochkov <dmitry.bochkov@gmail.com>
2020-01-04 00:02:37 +0200

    add vuetify and sass

---
+++ src/views/DisplayCurriculum.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="">
+    Display Curriculum
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DisplayCurriculum'
+
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/UpsertCurriculum.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="">
+    Upsert Curriculum
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UpsertCurriculum'
+
+}
+</script>
+
+<style scoped>
+</style>
